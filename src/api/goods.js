876cf6996397abff0237041c1c0a70f0c0f2d935
yucccc <fe@yucccc.com>
2018-04-09 22:08:01 +0800

    fix: 接口统一以api开头

--- src/api/goods.js
+++ src/api/goods.js
@@ -1,65 +1,66 @@
 import http from './public'
+const baseUrl = '/api'
 // 电脑列表
 export const getComputer = (params) => {
-  return http.fetchGet('/goods/computer', params)
+  return http.fetchGet(`${baseUrl}/goods/computer`, params)
 }
 // 获取购物车列表
 export const getCartList = (params) => {
-  return http.fetchPost('/users/cartList', params)
+  return http.fetchPost(`${baseUrl}/users/cartList`, params)
 }
 // 加入购物车
 export const addCart = (params) => {
-  return http.fetchPost('/goods/addCart', params)
+  return http.fetchPost(`${baseUrl}/goods/addCart`, params)
 }
 // 批量加入购物车
 export const addCartBatch = (params) => {
-  return http.fetchPost('/goods/addCartBatch', params)
+  return http.fetchPost(`${baseUrl}/goods/addCartBatch`, params)
 }
 // 删除购物车
 export const delCart = (params) => {
-  return http.fetchPost('/goods/delCart', params)
+  return http.fetchPost(`${baseUrl}/goods/delCart`, params)
 }
 // 编辑购物车
 export const cartEdit = (params) => {
-  return http.fetchPost('/users/cartEdit', params)
+  return http.fetchPost(`${baseUrl}/users/cartEdit`, params)
 }
 // 全选
 export const editCheckAll = (params) => {
-  return http.fetchPost('/users/editCheckAll', params)
+  return http.fetchPost(`${baseUrl}/users/editCheckAll`, params)
 }
 // 删除整条购物车
 export const cartDel = (params) => {
-  return http.fetchPost('/users/cartDel', params)
+  return http.fetchPost(`${baseUrl}/users/cartDel`, params)
 }
 // 获取用户地址
 export const addressList = (params) => {
-  return http.fetchPost('/users/addressList', params)
+  return http.fetchPost(`${baseUrl}/users/addressList`, params)
 }
 // 修改收货地址
 export const addressUpdate = (params) => {
-  return http.fetchPost('/users/addressUpdate', params)
+  return http.fetchPost(`${baseUrl}/users/addressUpdate`, params)
 }
 // 添加收货地址
 export const addressAdd = (params) => {
-  return http.fetchPost('/users/addressAdd', params)
+  return http.fetchPost(`${baseUrl}/users/addressAdd`, params)
 }
 // 删除收货地址
 export const addressDel = (params) => {
-  return http.fetchPost('/users/addressDel', params)
+  return http.fetchPost(`${baseUrl}/users/addressDel`, params)
 }
 // 支付
 export const payMent = (params) => {
-  return http.fetchPost('/users/payMent', params)
+  return http.fetchPost(`${baseUrl}/users/payMent`, params)
 }
 // 订单
 export const orderList = (params) => {
-  return http.fetchPost('/users/orderList', params)
+  return http.fetchPost(`${baseUrl}/users/orderList`, params)
 }
 // 商品详情
 export const productDet = (params) => {
-  return http.fetchGet('/goods/productDet', params)
+  return http.fetchGet(`${baseUrl}/goods/productDet`, params)
 }
 // 删除订单
 export const delOrder = (params) => {
-  return http.fetchPost('/users/delOrder', params)
+  return http.fetchPost(`${baseUrl}/users/delOrder`, params)
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,30 +1,31 @@
 import http from './public'
+const baseUrl = '/api'
 // 登陆
 export const userLogin = (params) => {
-  return http.fetchPost('/users/login', params)
+  return http.fetchPost(`${baseUrl}/users/login`, params)
 }
 // 退出登陆
 export const loginOut = (params) => {
-  return http.fetchPost('/users/loginOut', params)
+  return http.fetchPost(`${baseUrl}/users/loginOut`, params)
 }
 // 用户信息
 export const userInfo = (params) => {
-  return http.fetchPost('/users/userInfo', params)
+  return http.fetchPost(`${baseUrl}/users/userInfo`, params)
 }
 // 注册账号
 export const register = (params) => {
-  return http.fetchPost('/users/register', params)
+  return http.fetchPost(`${baseUrl}/users/register`, params)
 }
 // 上传图片
 export const upload = (params) => {
-  return http.fetchPost('/users/upload', params)
+  return http.fetchPost(`${baseUrl}/users/upload`, params)
 }
 // 修改头像
 export const updateheadimage = (params) => {
-  return http.fetchPost('/users/updateheadimage', params)
+  return http.fetchPost(`${baseUrl}/users/updateheadimage`, params)
 }
 // 首页接口
 export const productHome = (params) => {
-  return http.fetchGet('/goods/productHome', params)
+  return http.fetchGet(`${baseUrl}/goods/productHome`, params)
 }
 
